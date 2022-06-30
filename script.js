a66d7e82d032711c71d7c8b356ff56cdcbb16f9f
MariusPricope <mariuspricope66@gmai.com>
2022-06-30 17:50:53 +0300

    make card disapear when click on add or x button

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 const addListButton = document.getElementById("addList");
 const addListCard = document.getElementById("addListCard");
-var index = 0;
-var element;
-    
-const list = document.getElementById("list");
+var canAddList = true;
 
 //Here is a function which invoke herself
 (function() {
@@ -21,21 +18,54 @@ const list = document.getElementById("list");
         //shadowRoot is used to that code to be visible just in 
         //this.shadowRoot.innerHTML = `...`
         this.innerHTML = `
-            <input type="text" placeholder="Enter title" class="titleText">
-            <button class="addButton">ADD</button>
-            <button class="removeButton">X</button>
+            <input type="text" placeholder="Enter title" id="titleText">
+            <button id="addButton">ADD</button>
+            <button id="removeButton">X</button>
         `;
       }
     }
    window.customElements.define('add-listcard', AddListCard);
   })();
 
+//Delete card of "Add list"
+function DeleteCard( element)
+{
+    const parent = document.getElementById("list");
+    parent.removeChild(element);
+    canAddList = true;
+}
+
+//Execute this code when Add List button is pressed
 addListButton.addEventListener("click", function() {
-    element = document.createElement('add-listcard');
-    console.log(element);
-    element.innerHTML = "addCard";
-    element.setAttribute("id", "first");
-    document.getElementById("list").appendChild(element);
+    //if we don't have none card displyed, we can add card
+    if(canAddList) {
+      //We are creating the custom element
+      const element = document.createElement('add-listcard');
+      element.setAttribute("id", "addCard");
+      //Add element such as child in parent(list)
+      document.getElementById("list").appendChild(element);
+
+      const addButton = document.getElementById("addButton");
+      const removeButton = document.getElementById("removeButton");
+      //canAddList give false value cause' we add a card already
+      canAddList = false;
+
+      if(addButton)
+      {
+        addButton.addEventListener("click", function()
+        {
+          const title = document.getElementById("titleText");
+          DeleteCard(element);
+        })
+      }
+      if(removeButton)
+      {
+        removeButton.addEventListener("click", function()
+        {
+          DeleteCard(element);
+        })
+      }
+  }
 })
 
 
